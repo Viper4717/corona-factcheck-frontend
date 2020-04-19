aa371bfa79b38e1876524ca581fc9c69a7ce68a6
Viper4717 <aahad.24csedu.049@gmail.com>
2020-04-19 17:03:16 +0600

    Changed App.js to function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from "./components/header/Header"
 
-class App extends Component {
-  render() {
-    let links = [
-      { label: 'নীড়পাতা', link: '#home' },
-      { label: 'সত্য/মিথ্যা', link: '#about' },
-      { label: 'করোনা তথ্য', link: '#portfolio' },
-      { label: 'এক্সটেনশন', link: '#contact-us' },
-      { label: 'আমাদের সম্পর্কে', link: '#contact-us' },
-      { label: 'যোগাযোগ', link: '#contact-us' },
-    ];
+let links = [
+  { label: 'নীড়পাতা', link: '#home' },
+  { label: 'সত্য/মিথ্যা', link: '#about' },
+  { label: 'করোনা তথ্য', link: '#portfolio' },
+  { label: 'এক্সটেনশন', link: '#contact-us' },
+  { label: 'আমাদের সম্পর্কে', link: '#contact-us' },
+  { label: 'যোগাযোগ', link: '#contact-us' },
+];
 
-    return (
-      <div className="container center">
-        <Header links={links}/>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="container center">
+      <Header links={links}/>
+    </div>
+  );
 }
 
 export default App;
