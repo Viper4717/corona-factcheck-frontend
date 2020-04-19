d9575c8fecd9c6f6fd9205a836364222ffbf3f2f
Viper4717 <aahad.24csedu.049@gmail.com>
2020-04-19 19:04:07 +0600

    Minor changes in design

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from 'react';
-import Header from "./components/header/Header"
 import AboutUs from "./components/aboutUs/AboutUs"
-
-let links = [
-  { label: 'নীড়পাতা', link: '#home' },
-  { label: 'সত্য/মিথ্যা', link: '#about' },
-  { label: 'করোনা তথ্য', link: '#portfolio' },
-  { label: 'এক্সটেনশন', link: '#contact-us' },
-  { label: 'আমাদের সম্পর্কে', link: '#contact-us' },
-  { label: 'যোগাযোগ', link: '#contact-us' },
-];
 
 function App() {
   return (
     <div className="App">
-      <Header links={links}/>
       <AboutUs/>
     </div>
   );
--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Container from '@material-ui/core/container'
-import "./aboutUsStyle.css";
+import "./AboutUs.css";
 
 function AboutUs() {
   return (
