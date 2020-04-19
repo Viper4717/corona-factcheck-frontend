be8330e40154079412f7f6914ec9fe1a1bbf2be4
Viper4717 <aahad.24csedu.049@gmail.com>
2020-04-19 19:01:45 +0600

    Minor naming changes

--- src/components/extension/Extension.js
+++ src/components/extension/Extension.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import extensionStyle from "./extensionStyle.css"
-import { makeStyles } from '@material-ui/core/styles';
+import "./Extension.css"
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+
+/*
+    Extension page design
+    @author A.M. Aahad
+*/
 class Extension extends Component {
 
   render() {
