27910c33e5db44f1d106435b50f59daff8dceab2
Viper4717 <aahad.24csedu.049@gmail.com>
2020-04-19 14:43:06 +0600

    Created extension page component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
-import Header from "./components/header/header"
+import Extension from "./components/extension/Extension"
 
 class App extends Component {
   render() {
-    let links = [
-      { label: 'নীড়পাতা', link: '#home' },
-      { label: 'সত্য/মিথ্যা', link: '#about' },
-      { label: 'করোনা তথ্য', link: '#portfolio' },
-      { label: 'এক্সটেনশন', link: '#contact-us' },
-      { label: 'আমাদের সম্পর্কে', link: '#contact-us' },
-      { label: 'যোগাযোগ', link: '#contact-us' },
-    ];
-
     return (
       <div className="container center">
-        <Header links={links}/>
+        <Extension/>
       </div>
     );
   }
---
+++ src/components/extension/Extension.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import extensionStyle from "./extensionStyle.css"
+
+class Extension extends Component {
+  render() {
+    return (
+      <div className="extensionPage">
+        <div className="card">
+          <h2>Know about Corona</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Extension;
