34ddcf11d47eae1f296ff2399d022a4322fac904
Raheeb Hassan <raheeb998@gmail.com>
2020-04-19 21:53:51 +0600

    Changed container import

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Container from '@material-ui/core/container'
+import Container from '@material-ui/core/Container';
 import "./AboutUs.css";
 
 function AboutUs() {
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/container'
+import Container from '@material-ui/core/Container';
 
 
 /*
