c183bc8f5d98d9f1b4d24c773e468f7beb61f2bc
Raheeb Hassan <raheeb998@gmail.com>
2020-04-19 23:51:14 +0600

    linked header to appropriate pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Blog from './components/blog/Blog';
-import Extension from "./components/extension/Extension"
+import Extension from './components/extension/Extension';
 import AboutUs from './components/aboutUs/AboutUs';
 
 const headerLinks = [
-  { label: 'নীড়পাতা', link: '#home' },
-  { label: 'সত্য/মিথ্যা', link: '#about' },
-  { label: 'করোনা তথ্য', link: '#portfolio' },
-  { label: 'এক্সটেনশন', link: '#contact-us' },
-  { label: 'আমাদের সম্পর্কে', link: '#contact-us' },
+  { label: 'নীড়পাতা', link: '/' },
+  { label: 'সত্য/মিথ্যা', link: '' },
+  { label: 'করোনা তথ্য', link: '' },
+  { label: 'এক্সটেনশন', link: '/extension' },
+  { label: 'আমাদের সম্পর্কে', link: '/aboutUs' },
 ];
 
 function App() {
   return (
     <div className="App">
       <Header links={headerLinks} />
-      <Blog />
-      <Footer/>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Blog} />
+          <Route exact path="/aboutUs" component={AboutUs} />
+          <Route exact path="/extension" component={Extension} />
+        </Switch>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
