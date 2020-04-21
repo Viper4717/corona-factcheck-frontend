07c1764450ee58a2cd01616202584176550f567b
Raheeb Hassan <raheeb998@gmail.com>
2020-04-22 01:28:38 +0600

    Not working

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -3,14 +3,15 @@ import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
-import Axios from 'axios';
+import axios from 'axios';
 import Pagination from '@material-ui/lab/Pagination';
-import Post from './Post';
 import SearchIcon from '@material-ui/icons/Search';
 import TextField from '@material-ui/core/TextField';
+import Post from './Post';
 import { serverUrl } from '../../util';
 
 const cardHeight = '12rem';
+const postsPerPage = 2;
 
 const useStyles = makeStyles((theme) => ({
   Posts: {
@@ -26,124 +27,89 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const posts = [
-  {
-    id: 0,
-    title: 'Post 1',
-    date: 'Nov 10',
-    description:
-      "Multiple lines of text that form the lede's",
-    image: 'https://source.unsplash.com/random',
-    imgText: 'main image description',
-    link: 'https://google.com',
-    status: true,
-    statusText: 'সত্য',
-  },
-  {
-    id: 1,
-    title: 'Post 2',
-    date: 'Nov 12',
-    description:
-      "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
-    image: 'https://source.unsplash.com/random',
-    imageText: 'Image Text',
-    link: 'https://google.com',
-    status: false,
-    statusText: 'মিথ্যা',
-  },
-  {
-    id: 2,
-    title: 'Post 3',
-    date: 'Nov 11',
-    description:
-      "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
-    image: 'https://source.unsplash.com/random',
-    imageText: 'Image Text',
-    link: 'https://google.com',
-    status: true,
-    statusText: 'সত্য',
-  },
-  {
-    id: 3,
-    title: 'Post 4',
-    date: 'Nov 11',
-    description:
-      "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
-    image: 'https://source.unsplash.com/random',
-    imageText: 'Image Text',
-    link: 'https://google.com',
-    status: true,
-    statusText: 'সত্য',
-  },
-  {
-    id: 4,
-    title: 'Post 5',
-    date: 'Nov 11',
-    description:
-      "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
-    image: 'https://source.unsplash.com/random',
-    imageText: 'Image Text',
-    link: 'https://google.com',
-    status: false,
-    statusText: 'মিথ্যা',
-  },
-  {
-    id: 5,
-    title: 'Post 6',
-    date: 'Nov 11',
-    description:
-      "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
-    image: 'https://source.unsplash.com/random',
-    imageText: 'Image Text',
-    link: 'https://google.com',
-    status: true,
-    statusText: 'সত্য',
-  },
-];
 
-// function loadData(data, setData) {
+function loadData(data, setData) {
+  if (!data.postsLoaded) {
+    const searchText = encodeURI(data.searchText);
+    const searchUri = searchText ? `article_contains=${searchText}&` : '';
 
-//   // load featured posts
-//   Axios({
-//     method: 'GET',
-//     url: `${serverUrl}/featured-posts`,
-//   }).then((response) => {
-//     const featuredPosts = response.data.map((i) => ({
-//       id: i.id,
-//       date: new Date(i.post.created_at).toDateString(),
-//       title: i.post.title,
-//       description: i.post.desc,
-//       image: `${serverUrl}/${i.post.img.url}`,
-//       imageText: i.post.img.caption,
-//       link: `${serverUrl}/${i.post.img.url}`, // temporary replacement for link
-//       status: i.post.authenticity,
-//     }));
-//     setData({ ...data, featuredPosts });
-//   });
-// }
+    const start = data.pageNo * postsPerPage;
+    const limit = postsPerPage;
+    console.log({ start, limit });
+    const pagingUri = `_start=${start}&_limit=${limit}&`;
+
+    const countUrl = `${serverUrl}/posts/count?${searchUri}`;
+    const url = `${serverUrl}/posts?${searchUri}${pagingUri}`;
+
+    axios
+      .get(countUrl)
+      .then((response) => {
+        const count = parseInt(response.data, 10);
+        const totalPages = Math.floor(count, postsPerPage);
+        setData({ ...data, totalPages });
+        console.log('posts count loaded successfully');
+        console.log({count, totalPages}, data.totalPages);
+      })
+      .catch((error) => {
+        console.error(error);
+        console.log('failed to load posts count');
+      });
+
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(response);
+        const posts = response.data.map((i) => ({
+          id: i.id,
+          date: new Date(i.created_at).toDateString(),
+          title: i.title || '',
+          description: i.desc || '',
+          image: i.image
+            ? `${serverUrl}${i.image.formats ? i.image.formats.small.url : i.image.url}`
+            : null,
+          imageText: i.image ? (i.image.caption || i.image.alternativeText || null) : null,
+          link: `/article/${i.id}`,
+          status: i.authenticity,
+        }));
+        setData({ ...data, posts });
+        console.log('posts data loaded successfully');
+      })
+      .catch((error) => {
+        console.error(error);
+        console.log('failed to load posts data');
+      });
+  }
+}
 
 export default function Blog() {
   const classes = useStyles();
-  // const [data, setData] = useState({
-  //   featuredPosts: [],
-  // });
+  const [data, setData] = useState({
+    pageNo: 0,
+    totalPages: 0,
+    searchText: '',
+    posts: [],
+  });
 
-  // useEffect(() => {
-  //   loadData(data, setData);
-  // });
+  useEffect(() => {
+    loadData(data, setData);
+  }, [data.pageNo]);
 
   return (
     <div className={classes.Posts}>
       <CssBaseline />
       <Container maxWidth="lg">
         <main>
-          <div className="searchBox" style={{display: "flex", justifyContent: "center", marginBottom: "20px"}}>
-                <SearchIcon style={{marginTop: "3px"}}/>
-                <TextField id="full-width-text-field" placeholder="খুঁজুন" fullWidth
-                  style={{marginLeft: "10px", maxWidth: "400px"}} />
+          <div className="searchBox" style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+            <SearchIcon style={{ marginTop: '3px' }} />
+            <TextField
+              id="full-width-text-field"
+              placeholder="খুঁজুন"
+              fullWidth
+              style={{ marginLeft: '10px', maxWidth: '400px' }}
+            />
           </div>
           <Grid container spacing={7} justify="center">
-            {posts.map((post) => (
+            {data.posts.map((post) => (
               <Grid key={post.id} item xs={12} md={12} lg={12}>
                 <Post
                   key={post.id}
@@ -156,11 +122,12 @@ export default function Blog() {
         </main>
         <div className={classes.pageNum}>
           <Pagination
-            count={10}
+            count={data.totalPages}
             variant="outlined"
             shape="rounded"
-            onChange={(event, page) => {
-              console.log('Change Clicked', event, page);
+            onChange={(_event, page) => {
+              console.log({ totalPages: data.totalPages, page });
+              data.pageNo = page - 1;
             }}
           />
         </div>
