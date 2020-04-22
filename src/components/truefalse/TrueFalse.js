e48cf0d15c852551a58dfe6e11ab9b9095457f1d
Raheeb Hassan <raheeb998@gmail.com>
2020-04-23 01:31:57 +0600

    Minor warning fixes

--- src/components/truefalse/TrueFalse.js
+++ src/components/truefalse/TrueFalse.js
@@ -1,11 +1,11 @@
-import React, { Component, useState, useEffect } from 'react';
-import "./TrueFalse.css"
+import React, { useState, useEffect } from 'react';
+import './TrueFalse.css';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Axios from 'axios'
+import Axios from 'axios';
 import { serverUrl } from '../../util';
 
 /*
@@ -14,48 +14,51 @@ import { serverUrl } from '../../util';
 */
 
 function TrueFalse() {
-  const [data, setState] = useState({ 
-    downloadLinks: []
+  const [data, setState] = useState({
+    downloadLinks: [],
   });
 
   useEffect(() => {
     Axios({
       method: 'GET',
-      url: serverUrl + '/truefalse'
-    }).then(x => {
-      console.log(x)
+      url: `${serverUrl}/truefalse`,
+    }).then((x) => {
+      console.log(x);
       setState({
-        downloadLinks: x.data
-      })
-    })
+        downloadLinks: x.data,
+      });
+    });
   }, []);
-  let outputList = data.downloadLinks;
+  const outputList = data.downloadLinks;
   return (
     <div className="trueFalsePage">
-    {outputList.map(s => 
-      (
+      {outputList.map((s) => (
         <div className="trueFalsePage">
-        <center>
-        <Card className="card" variant="outlined">
-          <CardContent>
-            <Typography variant="h5" component="h2">
-              {s.title}
-            </Typography>
-          </CardContent>
-          <CardActions style={{justifyContent: "center"}}>
-            <Button className="downloadBtn" variant="contained" 
-              style={{backgroundColor: "#04b376"}} size="large"
-              href={s.url}>
-              <Typography style={{color: "#ffffff", fontWeight: "bold", fontSize: 18}}>
-                ডাউনলোড
-              </Typography>
-            </Button>
-          </CardActions>
-        </Card>
-        </center>
-      </div>
-    ))}
-  </div>
+          <center>
+            <Card className="card" variant="outlined">
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  {s.title}
+                </Typography>
+              </CardContent>
+              <CardActions style={{ justifyContent: 'center' }}>
+                <Button
+                  className="downloadBtn"
+                  variant="contained"
+                  style={{ backgroundColor: '#04b376' }}
+                  size="large"
+                  href={s.url}
+                >
+                  <Typography style={{ color: '#ffffff', fontWeight: 'bold', fontSize: 18 }}>
+                    ডাউনলোড
+                  </Typography>
+                </Button>
+              </CardActions>
+            </Card>
+          </center>
+        </div>
+      ))}
+    </div>
   );
 }
 
