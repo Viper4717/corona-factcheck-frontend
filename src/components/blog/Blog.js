ad652732200840b7cd1a9542b3619d37b5b13b89
Raheeb Hassan <raheeb998@gmail.com>
2020-04-22 13:48:02 +0600

    Remove pagebar when only one page

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -149,20 +149,28 @@ export default function Blog({ location, history }) {
             </Grid>
           ))}
         </Grid>
-        <div className={classes.pageNum}>
-          <Pagination
-            shape="rounded"
-            count={totalPages}
-            page={state.pageNo + 1}
-            onChange={(_event, newPage) => {
-              // handle page change
-              const newUrl = `blog?${getQueryString(state.searchText, newPage - 1)}`;
-              // eslint-disable-next-line react/prop-types
-              history.push(newUrl);
-              setState((prevState) => ({ ...prevState, pageNo: newPage - 1 }));
-            }}
-          />
-        </div>
+        {() => {
+          // only render pagination if there are more than 1 page
+          if (totalPages > 1) {
+            return (
+              <div className={classes.pageNum}>
+                <Pagination
+                  shape="rounded"
+                  count={totalPages}
+                  page={state.pageNo + 1}
+                  onChange={(_event, newPage) => {
+                    // handle page change
+                    const newUrl = `blog?${getQueryString(state.searchText, newPage - 1)}`;
+                    // eslint-disable-next-line react/prop-types
+                    history.push(newUrl);
+                    setState((prevState) => ({ ...prevState, pageNo: newPage - 1 }));
+                  }}
+                />
+              </div>
+            );
+          }
+          return null;
+        }}
       </Container>
     </div>
   );
