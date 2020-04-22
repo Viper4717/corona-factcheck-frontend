a9cb8f330b34c2a1218729646e9e6043f0974d78
Raheeb Hassan <raheeb998@gmail.com>
2020-04-22 16:32:02 +0600

    Removed eslint/prop-types warning

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
