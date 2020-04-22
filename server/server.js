c4a9377c0e4f648daf5d244ba018ee940dedec05
Raheeb Hassan <raheeb998@gmail.com>
2020-04-23 01:29:49 +0600

    Fixed server not working issue

--- server/server.js
+++ server/server.js
@@ -2,12 +2,15 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
+
+const buildPath = path.join(__dirname, '..', 'build');
+
 const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+app.use(express.static(buildPath));
+
+app.get('/*', (_req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'));
 });
 
 app.listen(port, () => {
